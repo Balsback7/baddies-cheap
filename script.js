35210809c6e1f01a6bc5d010f44b570bf9ae5d25
Balsback7 <c0mb026f@gmail.com>
2026-01-15 16:42:13 -0500

    Cleanup: Remove filter function and rainbow-bg div
    Removed unused filter function and a div element.

--- script.js
+++ script.js
@@ -5,18 +5,12 @@ function render(list) {
   list.forEach(p => {
     container.innerHTML += `
       <a class="card" href="product.html?id=${p.id}">
-        <div class="rainbow-bg"></div>
         <img src="${p.image}">
         <h3>${p.name}</h3>
         <p class="price">$${p.price} <span>$${p.oldPrice}</span></p>
       </a>
     `;
   });
-}
-
-function filter(cat) {
-  if (cat === "all") return render(products);
-  render(products.filter(p => p.category === cat));
 }
 
 render(products);
