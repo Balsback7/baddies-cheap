7b297350003b839c1f60674e60e22d5173b16fe2
Balsback7 <c0mb026f@gmail.com>
2026-01-15 16:22:07 -0500

    Add product rendering and filtering functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const container = document.getElementById("products");
 
+function render(list) {
+  container.innerHTML = "";
+  list.forEach(p => {
+    container.innerHTML += `
+      <a class="card" href="product.html?id=${p.id}">
+        <img src="${p.image}">
+        <h3>${p.name}</h3>
+        <p class="price">$${p.price} <span>$${p.oldPrice}</span></p>
+      </a>
+    `;
+  });
+}
+
+function filter(cat) {
+  if (cat === "all") return render(products);
+  render(products.filter(p => p.category === cat));
+}
+
+render(products);
