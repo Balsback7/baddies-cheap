70ff2c4d9ba5ea68d6e5f3843bd46a042df187d1
Balsback7 <c0mb026f@gmail.com>
2026-01-15 16:51:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function render(list) {
   list.forEach(p => {
     container.innerHTML += `
       <a class="card" href="product.html?id=${p.id}">
+        <div class="stock">In Stock</div>
+
         <img src="${p.image}">
         <h3>${p.name}</h3>
         <p class="price">$${p.price} <span>$${p.oldPrice}</span></p>
