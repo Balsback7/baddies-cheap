9d09dfd7980656103908a565a3feaf7c081caf08
Balsback7 <c0mb026f@gmail.com>
2026-08-18 11:24:38 -0400

    Create value.lua

---
+++ value.lua
@@ -0,0 +1,699 @@
+local Items = {
+    ["Blast Bow"] = {
+        Value = 13000,
+        TokenValue = 12000,
+        Category = "Weapon",
+    },
+    ["Brass Knuckles"] = {
+        Value = 7500,
+        TokenValue = 6000,
+        Category = "Weapon",
+    },
+    ["Bunny Shoes"] = {
+        Value = 9500,
+        TokenValue = 11000,
+        Category = "Weapon",
+    },
+    ["Cannon"] = {
+        Value = 5500,
+        TokenValue = 5000,
+        Category = "Weapon",
+    },
+    ["Chainsaw"] = {
+        Value = 5000,
+        TokenValue = 2500,
+        Category = "Weapon",
+    },
+    ["Chakram"] = {
+        Value = 2500,
+        TokenValue = 900,
+        Category = "Weapon",
+    },
+    ["Champion Gloves"] = {
+        Value = 4500,
+        TokenValue = 3000,
+        Category = "Weapon",
+    },
+    ["Cupid's Bow"] = {
+        Value = 80000,
+        TokenValue = 39000,
+        Category = "Weapon",
+    },
+    ["Dance Bomb"] = {
+        Value = 7500,
+        TokenValue = 5000,
+        Category = "Weapon",
+    },
+    ["Dog Purse"] = {
+        Value = 20000,
+        TokenValue = 17000,
+        Category = "Weapon",
+    },
+    ["Fan of Requiem"] = {
+        Value = 5500,
+        TokenValue = 2400,
+        Category = "Weapon",
+    },
+    ["Freeze Gun"] = {
+        Value = 6000,
+        TokenValue = 8000,
+        Category = "Weapon",
+    },
+    ["Ghostly Gloves"] = {
+        Value = 800000,
+        TokenValue = 650000,
+        Category = "Weapon",
+    },
+    ["Glitter Blue Spray"] = {
+        Value = 130000,
+        TokenValue = 80000,
+        Category = "Weapon",
+    },
+    ["Glitter Bomb"] = {
+        Value = 750000,
+        TokenValue = 600000,
+        Category = "Weapon",
+    },
+    ["Golden Snowball Launcher"] = {
+        Value = 1500,
+        TokenValue = 1500,
+        Category = "Weapon",
+    },
+    ["Grapple Hook"] = {
+        Value = 20000,
+        TokenValue = 16000,
+        Category = "Weapon",
+    },
+    ["Gravity Gun"] = {
+        Value = 60000,
+        TokenValue = 62000,
+        Category = "Weapon",
+    },
+    ["Grenade Launcher"] = {
+        Value = 105000,
+        TokenValue = 75000,
+        Category = "Weapon",
+    },
+    ["Grim Reaper Cloak"] = {
+        Value = 28000,
+        TokenValue = 20000,
+        Category = "Weapon",
+    },
+    ["Harpoon"] = {
+        Value = 1000,
+        TokenValue = 1000,
+        Category = "Weapon",
+    },
+    ["Ice Crown Queen"] = {
+        Value = 110000,
+        TokenValue = 60000,
+        Category = "Weapon",
+    },
+    ["Ice Katana"] = {
+        Value = 105000,
+        TokenValue = 68000,
+        Category = "Weapon",
+    },
+    ["Inferno Blade"] = {
+        Value = 6500,
+        TokenValue = 4500,
+        Category = "Weapon",
+    },
+    ["Katana & Kunai Set"] = {
+        Value = 4500,
+        TokenValue = 800,
+        Category = "Weapon",
+    },
+    ["Kitty Purse"] = {
+        Value = 3500,
+        TokenValue = 3500,
+        Category = "Weapon",
+    },
+    ["Love Me Hate Me Taser"] = {
+        Value = 115000,
+        TokenValue = 80000,
+        Category = "Weapon",
+    },
+    ["Loveboard"] = {
+        Value = 18000,
+        TokenValue = 13500,
+        Category = "Weapon",
+    },
+    ["Medic Spray"] = {
+        Value = 45000,
+        TokenValue = 30000,
+        Category = "Weapon",
+    },
+    ["Nunchucks"] = {
+        Value = 4000,
+        TokenValue = 1500,
+        Category = "Weapon",
+    },
+    ["Poison Knuckles"] = {
+        Value = 3000,
+        TokenValue = 2200,
+        Category = "Weapon",
+    },
+    ["Rainbow Portal Gun"] = {
+        Value = 15000,
+        TokenValue = 9500,
+        Category = "Weapon",
+    },
+    ["Reindeer Purse"] = {
+        Value = 5000,
+        TokenValue = 2000,
+        Category = "Weapon",
+    },
+    ["Riot Shield"] = {
+        Value = 9000,
+        TokenValue = 4000,
+        Category = "Weapon",
+    },
+    ["Sakura Blade"] = {
+        Value = 9000,
+        TokenValue = 6000,
+        Category = "Weapon",
+    },
+    ["Sakura Sword and Shield Set"] = {
+        Value = 20000,
+        TokenValue = 15000,
+        Category = "Weapon",
+    },
+    ["Santa's Naughty or Nice Launcher"] = {
+        Value = 190000,
+        TokenValue = 155000,
+        Category = "Weapon",
+    },
+    ["Scythe"] = {
+        Value = 230000,
+        TokenValue = 160000,
+        Category = "Weapon",
+    },
+    ["Sledgehammer"] = {
+        Value = 2100,
+        TokenValue = 1450,
+        Category = "Weapon",
+    },
+    ["Spear"] = {
+        Value = 10000,
+        TokenValue = 3000,
+        Category = "Weapon",
+    },
+    ["Spiked Kitty Stanli"] = {
+        Value = 45000,
+        TokenValue = 30000,
+        Category = "Weapon",
+    },
+    ["Spiked Knuckles"] = {
+        Value = 135000,
+        TokenValue = 78000,
+        Category = "Weapon",
+    },
+    ["Spiked Nightmare Purse"] = {
+        Value = 35000,
+        TokenValue = 20000,
+        Category = "Weapon",
+    },
+    ["Trashbin Disguise"] = {
+        Value = 15500,
+        TokenValue = 16000,
+        Category = "Weapon",
+    },
+    ["Trident"] = {
+        Value = 85000,
+        TokenValue = 40000,
+        Category = "Weapon",
+    },
+    ["Turkey Skewers"] = {
+        Value = 2500,
+        TokenValue = 1200,
+        Category = "Weapon",
+    },
+    ["Twin Blade"] = {
+        Value = 6500,
+        TokenValue = 4000,
+        Category = "Weapon",
+    },
+    ["Vine Bomb"] = {
+        Value = 10000,
+        TokenValue = 4300,
+        Category = "Weapon",
+    },
+    ["Water Fan"] = {
+        Value = 9000,
+        TokenValue = 6000,
+        Category = "Weapon",
+    },
+    ["Wingblades"] = {
+        Value = 40000,
+        TokenValue = 16000,
+        Category = "Weapon",
+    },
+    ["Witch Scepter"] = {
+        Value = 4500,
+        TokenValue = 1800,
+        Category = "Weapon",
+    },
+    ["Yin Yang Knives"] = {
+        Value = 5500,
+        TokenValue = 4000,
+        Category = "Weapon",
+    },
+    ["404 Not Found Board"] = {
+        Value = 1700,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["4th of July Hoverboard"] = {
+        Value = 2000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Angel Wings"] = {
+        Value = 5500,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Angelic Board"] = {
+        Value = 20000,
+        TokenValue = 15000,
+        Category = "Loveboard",
+    },
+    ["BadKittie Board"] = {
+        Value = 2000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Bath Tub Hoverboard"] = {
+        Value = 6000,
+        TokenValue = 2500,
+        Category = "Loveboard",
+    },
+    ["Boxing Ring Hoverboard"] = {
+        Value = 12500,
+        TokenValue = 3000,
+        Category = "Loveboard",
+    },
+    ["Carnival Board"] = {
+        Value = 5000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Cauldron Hoverboard"] = {
+        Value = 1500,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Cosmic Butterfly Board"] = {
+        Value = 1650,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Credit Card Hoverboard"] = {
+        Value = 25000,
+        TokenValue = 7000,
+        Category = "Loveboard",
+    },
+    ["Crystal Heart Board"] = {
+        Value = 1800,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Cursed Board"] = {
+        Value = 4150,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Devil Wings"] = {
+        Value = 2200,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Disco Dash Board"] = {
+        Value = 5000,
+        TokenValue = 4000,
+        Category = "Loveboard",
+    },
+    ["Dragon Hoverboard"] = {
+        Value = 2000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Dream Dust Glide Board"] = {
+        Value = 1100,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Ember Log Hoverboard"] = {
+        Value = 1000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Ethereal Feast Glide Board"] = {
+        Value = 7500,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Fire Hoverboard"] = {
+        Value = 2000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Frostwind Glider Board"] = {
+        Value = 10000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Ghastly Glider Board"] = {
+        Value = 7000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Good Kitty Hoverboard"] = {
+        Value = 1650,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Graduation Hat Hoverboard"] = {
+        Value = 4000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Ice cream Sandwich Board"] = {
+        Value = 7500,
+        TokenValue = 2500,
+        Category = "Loveboard",
+    },
+    ["Juicy Hoverboard"] = {
+        Value = 2000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Justice Hoverboard"] = {
+        Value = 1530,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Kiss Kiss Hoverboard"] = {
+        Value = 6500,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Mallow Glide Board"] = {
+        Value = 2500,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Mystic Jungle Board"] = {
+        Value = 6000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Neon Heart Hoverboard"] = {
+        Value = 2900,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Orbit Hoverboard"] = {
+        Value = 2500,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Pink Star Board"] = {
+        Value = 22000,
+        TokenValue = 8000,
+        Category = "Loveboard",
+    },
+    ["Police Hoverboard"] = {
+        Value = 5000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Popstar Hoverboard"] = {
+        Value = 10000,
+        TokenValue = 6000,
+        Category = "Loveboard",
+    },
+    ["Queen's Throne"] = {
+        Value = 2000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Reindeer Hoverboard"] = {
+        Value = 4000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Rocket Hoverboard"] = {
+        Value = 1500,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Royal Throne Hoverboard"] = {
+        Value = 6500,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Shark Wave Hoverboard"] = {
+        Value = 2000,
+        TokenValue = 850,
+        Category = "Loveboard",
+    },
+    ["Surf Hoverboard"] = {
+        Value = 1150,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Surf's Up Hoverboard"] = {
+        Value = 25000,
+        TokenValue = 19000,
+        Category = "Loveboard",
+    },
+    ["Toast Hoverboard"] = {
+        Value = 7500,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["UFO Hoverboard"] = {
+        Value = 1500,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Void Demon Wings"] = {
+        Value = 85000,
+        TokenValue = 30000,
+        Category = "Loveboard",
+    },
+    ["Winter Snowflake Hoverboard"] = {
+        Value = 1000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Witches Board"] = {
+        Value = 3000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Witchy Broom Board"] = {
+        Value = 1800,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Y&Y Board"] = {
+        Value = 6000,
+        TokenValue = nil,
+        Category = "Loveboard",
+    },
+    ["Air Strike Finisher"] = {
+        Value = 17500,
+        TokenValue = 10000,
+        Category = "Finisher",
+    },
+    ["Arise Finisher"] = {
+        Value = 3000,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Ball Cup Finisher"] = {
+        Value = 3500,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Boba Tea Finisher"] = {
+        Value = 1000,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Breakdance Finisher"] = {
+        Value = 1500,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Bubble Pop Obliteration Finisher"] = {
+        Value = 1750,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Candy Crush"] = {
+        Value = 1500,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Cheerleader Finisher"] = {
+        Value = 2100,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Cosmic Finisher"] = {
+        Value = 2500,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Crushing Love"] = {
+        Value = 10000,
+        TokenValue = 6500,
+        Category = "Finisher",
+    },
+    ["Cupid Bow Finisher"] = {
+        Value = 4000,
+        TokenValue = 2000,
+        Category = "Finisher",
+    },
+    ["Dog Purse Finisher"] = {
+        Value = 1100,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Doom Finisher"] = {
+        Value = 4000,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Dunked On Finisher"] = {
+        Value = 2500,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Fart Finisher"] = {
+        Value = 2000,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Food Fight Finisher"] = {
+        Value = 19000,
+        TokenValue = 15000,
+        Category = "Finisher",
+    },
+    ["Gun Finisher"] = {
+        Value = 3500,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Killer Bunny Finisher"] = {
+        Value = 5000,
+        TokenValue = 1500,
+        Category = "Finisher",
+    },
+    ["Love Bomb Finisher"] = {
+        Value = 8000,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Panda Finisher"] = {
+        Value = 4000,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Rainbow Aerial Finisher"] = {
+        Value = 5500,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Rockstar Finisher"] = {
+        Value = 6500,
+        TokenValue = 5000,
+        Category = "Finisher",
+    },
+    ["Sakura Finisher"] = {
+        Value = 5500,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Solar Beam Finisher"] = {
+        Value = 2200,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Vanity Vortex Finisher"] = {
+        Value = 5500,
+        TokenValue = nil,
+        Category = "Finisher",
+    },
+    ["Black Flame Stomp"] = {
+        Value = 33000,
+        TokenValue = 13500,
+        Category = "Stomp",
+    },
+    ["Frost Stomp"] = {
+        Value = 3800,
+        TokenValue = nil,
+        Category = "Stomp",
+    },
+    ["Inferno Stomp"] = {
+        Value = 590000,
+        TokenValue = nil,
+        Category = "Stomp",
+    },
+    ["Mischief Stomp"] = {
+        Value = 8500,
+        TokenValue = 3500,
+        Category = "Stomp",
+    },
+    ["Royal Stomp"] = {
+        Value = 1300,
+        TokenValue = nil,
+        Category = "Stomp",
+    },
+    ["Angel Style"] = {
+        Value = 22000,
+        TokenValue = 7500,
+        Category = "Fighting Style",
+    },
+    ["Beast Style"] = {
+        Value = 15500,
+        TokenValue = 9000,
+        Category = "Fighting Style",
+    },
+    ["Fantasy Style"] = {
+        Value = 6000,
+        TokenValue = 3500,
+        Category = "Fighting Style",
+    },
+    ["Feral Frenzy Style"] = {
+        Value = 3500,
+        TokenValue = nil,
+        Category = "Fighting Style",
+    },
+    ["Glitter Style"] = {
+        Value = 3500,
+        TokenValue = 3600,
+        Category = "Fighting Style",
+    },
+    ["Goth Style"] = {
+        Value = 6000,
+        TokenValue = 7500,
+        Category = "Fighting Style",
+    },
+    ["Heartbreaker Style"] = {
+        Value = 340000,
+        TokenValue = 240000,
+        Category = "Fighting Style",
+    },
+    ["Princess Power Style"] = {
+        Value = 15000,
+        TokenValue = 5500,
+        Category = "Fighting Style",
+    },
+}
+
+return Items
